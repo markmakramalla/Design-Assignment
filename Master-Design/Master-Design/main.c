64f17cee417407fb8c9134a549c55867ee194978
markmakramalla <61844837+markmakramalla@users.noreply.github.com>
2020-03-07 16:40:25 -0400

    Added Echolocator to Code
    Echolocator code is below the keypad code

--- Master-Design/Master-Design/main.c
+++ Master-Design/Master-Design/main.c
@@ -188,3 +188,135 @@ Curator code --> make new code array, inside the function we disable alarm.
 Silent alarm code --> make new code array, inside function, enable silent alarm.
 */
 
+// ECHOLOCATOR
+
+
+/*
+ * Main.c
+ *
+ * Author : Mark LeBlanc, Dalhousie University
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <stdio.h>
+
+
+volatile unsigned char MIP;
+volatile unsigned int ECHOHigh, ECHOLow, ECHOHighPipe;
+volatile unsigned int TimeOutCnt,Tick;
+volatile unsigned int distance;
+
+
+/***************************************************************************************
+When the echo length is longer than the counter duration, we use an additional byte to 
+indicate how many times we reach the maximum value. 
+***************************************************************************************/
+ISR (TIMER1_OVF_vect) { // For long ECHO's
+    if(ECHOHighPipe >= 2) {
+        TIMSK1 = 0; // No further interrupts.
+        TCCR1B = 0; // Stop Clock
+        MIP = 0xFF; // End Measurement
+    }
+    
+    ECHOHighPipe++; // Add 1 to High byte.
+}
+
+/***************************************************************************************
+Interrupt service routine called when the input capture pin state is changed
+***************************************************************************************/
+ISR (TIMER1_CAPT_vect) {    // Start and Stop ECHO measurement;
+    if((TCCR1B & (1<<ICES1)) != 0) { // a rising edge has been detected
+        TCCR1B |= (1<<CS01) | (1<<CS00);    // Start counting with ck/8;
+        TCCR1B &= ~(1<<ICES1);  // Configure Negative Edge Capture for end of echo pulse.
+    }
+    
+    else {                      // a falling edge has been detected
+        ECHOLow = TCNT1;
+        ECHOHigh = ECHOHighPipe;
+        TIMSK1 = (1<<OCIE1B);   // Enables the Compare B interrupt for POST Trigger Delay: Approx 10mS
+        TCNT1 = 0;
+    }
+}
+
+
+/***************************************************************************************
+Interrupt service routine called when the counter 1 has reached the compare value
+***************************************************************************************/
+ISR (TIMER1_COMPB_vect) {   // Compare B: Post ECHO delay 10mS
+
+
+
+    MIP = 0;    // End Measurement
+}
+
+/***************************************************************************************
+Interrupt service routine called when the counter 1 has reached the compare value
+***************************************************************************************/
+ISR (TIMER1_COMPA_vect) {   // Compare A : End of Trigger Pulse
+    PORTB &= ~(1<<PB1);
+    TIMSK1 = (1<<ICIE1)|(1<<TOIE1); // enables the T/C1 Overflow and Capture interrupt;
+    TCCR1B = (1<<ICES1);            // Set Positive edge for capture but Don't count yet
+
+
+}
+
+/******************************************************************************
+******************************************************************************/
+void Trigger( void ) {      // Config Timer 1 for 10 to 15uS pulse.
+    if(MIP == 0) {  // Don't allow re-trigger.
+        MIP = 1;                // Set Measurement in progress FLAG
+        DDRB |= (1<<PB1);       // PB1 as Output for Trigger pulse.
+        DDRB &= ~(1<<PB0);      // PB0 as Input for Input Capture (ECHO).
+        
+        TCNT1 = 0;              // Clear last Echo times.
+        ECHOHighPipe = 0;
+        
+        OCR1B = 10100;          // 10 mS Post echo Delay
+        OCR1A = 12;             // 10 us Trigger length.
+
+        PORTB |= (1<<PB1);      // Start Pulse.
+
+        TIFR1 = 0xFF;           //  Clear all timer interrupt flags
+        TCCR1A = 0;   // Timer mode with Clear Output on Match
+        TCCR1B = (1<<WGM12) | (1<<CS00) | (1<<CS01);  // Counting with CKio/8 CTC Mode enabled
+        TIMSK1 = (1<<OCIE1A);   // enables the T/C1 Overflow, Compare A, and Capture interrupt;
+    }
+    
+}
+
+/******************************************************************************
+******************************************************************************/
+int main(void){
+    uint8_t key;
+    
+    init_uart();
+
+    sei();
+    
+    printf("Hello\n\n");
+
+    while (1 == 1) {
+    
+    key = getchar();
+    
+    switch(key) {
+        case 13:
+            printf("\n");
+            break;
+
+        case 'T':
+        case 't':
+            Trigger();
+            while (MIP == 1){};
+            distance =((((1.0/16000000.0)*64.0*65535.0/58.0*1000000.0))*ECHOHigh) + (4520.0*ECHOLow/65535.0) ; // distance of 1 echohigh in cm
+            printf("Echo is %d %d\n\n", ECHOHigh, ECHOLow);
+            printf("distance in cm is %d \n", distance);
+            break;
+        
+        }
+    }
+}
